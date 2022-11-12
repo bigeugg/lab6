3ff84d4739df1665cc9ca822a9b7eecfb87aaa37
Eugene Desktop <elee331@my.bcit.ca>
2022-11-11 20:57:30 -0800

    made a loop that iterates through the course list

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,12 @@ do {
     console.log(input);
 }
 while (input.length != 4 || !NaN(Number(input)))
+
+
+for (let i of courseList) {
+    if (i.contains(input)){
+        console.log (`Yes I am taking the course: ${courseList.code} - ${courseList.name}`)
+    } else {
+        (i.unshift (code === (input) && (name === null)));
+    }
+}
