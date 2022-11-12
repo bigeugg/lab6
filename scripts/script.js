e567a4e11d560bde9b049b1b9f911f24bd406731
Eugene Desktop <elee331@my.bcit.ca>
2022-11-11 20:19:49 -0800

    defined a constant array with my course lists

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+
+const courseList = [
+    {code: "ACIT 1620", 
+    name: "Web Fundamental Technologies"},
+
+    {code: "ACIT 1420", 
+    name: "Introduction to Systems Administration"},
+
+    {code: "ACIT 1630",
+     name: "Database Systems"},
+     
+]
